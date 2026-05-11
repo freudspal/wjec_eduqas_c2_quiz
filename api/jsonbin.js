4c01f617c1d27fa5fb506b0d2a8f732d8a32231f
freudspal <siemieniecs@chichester.ac.uk>
2026-05-11 17:58:20 +0100

    Update jsonbin.js

--- api/jsonbin.js
+++ api/jsonbin.js
@@ -198,7 +198,7 @@ FORMAT EXACTLY:
 `;
 
     const r = await fetch(
-  `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
+  `https://generativelanguage.googleapis.com/v1beta/models/gemma-4-26b-a4b-it:generateContent?key=${GEMINI_API_KEY}`,
   {
     method: "POST",
     headers: {
